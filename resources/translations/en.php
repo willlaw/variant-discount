85b1f96ab6a9a578a147964648bebf0102037686
willlaw <will.law@me.com>
2017-04-11 14:37:09 +0100

    Add translations

---
+++ resources/translations/en.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Variant Discount plugin for Craft CMS
+ *
+ * Variant Discount Translation
+ *
+ * @author    Luke Holder
+ * @copyright Copyright (c) 2017 Luke Holder
+ * @link      http://craftcms.stackexchange.com/users/91/luke-holder
+ * @package   VariantDiscount
+ * @since     1.0.0
+ */
+
+return array(
+    'Translate me' => 'To this',
+);
